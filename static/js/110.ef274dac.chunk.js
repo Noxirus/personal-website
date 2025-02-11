88b3dd74c724687a7fb808683d4cc35304046893
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2025-02-11 11:30:37 -0700

    Updates

---
+++ static/js/110.ef274dac.chunk.js
@@ -0,0 +1,36 @@
+"use strict";(self.webpackChunkpersonal_website=self.webpackChunkpersonal_website||[]).push([[110],{6584:(e,a,i)=>{i.r(a),i.d(a,{default:()=>D});var t=i(9867),n=i(9637),s=i(1767),l=i(8912),o=i(6295),r=i(6521),d=i(2834);const c=e=>{let{imgRef:a,altText:i,delay:n}=e;const s=(0,t.useRef)(null),[l]=(0,t.useState)((0,o.y)(s));return(0,d.jsx)(g,{trigger:l.GetIsIntersecting(),ref:s,delay:n,title:i,src:a,alt:i})},g=s.ZP.img`
+border-width: 2px;
+border-style: solid;
+border-radius: 15px;
+width: 75px;
+height: 75px;
+min-width: 50px;
+min-height: 50px;
+margin: 5px;
+padding: 5px;
+background-color: white;
+border-color: ${l.V.primary} ${l.V.secondary} ${l.V.grey};
+transform: scale(1, 1);
+animation: ${e=>e.trigger&&s.iv`${r.J} .3s linear ${e=>e.delay?e.delay:"0s"}`};
+animation-fill-mode: forwards;
+`;const m=i.p+"static/media/c-sharp-c.93ba21ebf5c7f4e5fb94538c4c948fec.svg";const p=i.p+"static/media/unity-3d.5b2834b4709ba7fa0b85d2fc19ec317b.svg";const x=i.p+"static/media/javascript-js.63b93412a24001eff2acc22e0cb40276.svg";const f=i.p+"static/media/github.0f0cfa96e2cfb6446e8f50efde52d555.svg";const b=i.p+"static/media/file-type-firebase.d770ed71d4d33ffa60f8cae9b7ba0350.svg";const u=i.p+"static/media/react.0cc978f43ea437de003e85712225e83b.svg";const h=i.p+"static/media/visual-studio.dd5ad295a13f150dd8e120c49d910823.svg";const y=i.p+"static/media/unreal-engine.986da142dfb1b0aaca8e11871b92c661.svg";const j=i.p+"static/media/CPlusPlusIcon.f00d9334f1e6c0da4960a74ebe7769cb.svg";const v=i.p+"static/media/node-js.a7647021075b20ebcf315dd54793112d.svg";var w=i(3890),C=i(7878);const P=e=>{let{details:a}=e;return(0,d.jsxs)(w.rg,{children:[(0,d.jsxs)(w.Qh,{children:[(0,d.jsxs)(k,{children:[a.name," ",a.link&&(0,d.jsx)(G,{icon:C.t3U,onClick:()=>window.open(a.link)})]}),(0,d.jsx)("ul",{children:a.bulletPoints.map(((e,a)=>(0,d.jsx)(w.oH,{children:e},a)))})]}),a.img&&(0,d.jsx)(w.Qh,{xl:"5",lg:"6",sm:"12",children:(0,d.jsx)(w.fb,{children:(0,d.jsx)(w.xu,{alt:a.alt,title:a.name,src:a.img,onClick:()=>window.open(a.link)})})})]})},G=(0,s.ZP)(w.ur)`
+color: ${l.V.tertiary}
+`,k=s.ZP.h3`
+font-size: clamp(20px, 6vw, 35px);
+color: ${l.V.secondary};
+animation: ${e=>e.animation?e.animation:""};
+${e=>e.initialState?e.initialState:""};
+`,A=i.p+"static/media/BVCLogo.08e76198237570929a73.webp",R=i.p+"static/media/CGDALogo.521015525f56902f3337.webp",T=i.p+"static/media/NiceAxeGamesLogo.6ce8ae76f11a498fc1f1.webp",U={name:"Bow Valley College",img:A,link:"https://bowvalleycollege.ca/programs-courses/centre-for-entertainment-arts/advanced-game-development",bulletPoints:["Teaching in the Advanced Game Development Program.","Game Programming with C# and C++ in the Unity and Unreal Engines.","Source control tools including, Git, Perforce and GitKraken.","Unreal Engine blueprints, game systems, animations and AI behavior."],alt:"Bow Valley College Advanced Game Development Program."},$={name:"Calgary Game Developers Association",img:R,link:"https://www.calgary.games/",bulletPoints:["Director on the Board.","Hosting monthly meetup events focused on the Unreal Engine.","Actively engaged in community building."],alt:"Calgary Game Developers Association is a non profit organization determined to build a stronger gaming community."},E={name:"Nice Axe Games",img:T,link:"https://www.niceaxegames.com/",bulletPoints:["Unreal Engine Programmer and Designer.","Assisting in the development of online game systems using Unreal Engine Blueprints."],alt:"Nice Axe Games, building fun multiplayer experiences with the Unreal Engine"},V={name:"Additional Skills",bulletPoints:["Developing Unreal Engine games using C++ and Perforce.","Building Unity WebGL/Android games utilizing C#/.NET, Google Firebase Cloud, Google Play Console, and Google AdMob Monetization.","Developing websites using React, JavaScript, Google Firebase Cloud, NPM, Express, Node.js, MongoDB, JSON, HTML, and CSS."]},D=()=>(0,d.jsxs)(w.m5,{id:"experience",padding:"200px",children:[(0,d.jsx)(n.X2,{children:(0,d.jsx)(n.JX,{children:(0,d.jsx)(w.EZ,{children:"Experience"})})}),(0,d.jsx)("br",{}),(0,d.jsx)(n.X2,{children:(0,d.jsxs)(S,{children:[(0,d.jsx)(c,{delay:".00s",imgRef:y,altText:"Unreal"}),(0,d.jsx)(c,{delay:".05s",imgRef:j,altText:"C++"}),(0,d.jsx)(c,{delay:".1s",imgRef:p,altText:"Unity"}),(0,d.jsx)(c,{delay:".15s",imgRef:m,altText:"C#"}),(0,d.jsx)(c,{delay:".2s",imgRef:x,altText:"Javascript"}),(0,d.jsx)(c,{delay:".25s",imgRef:h,altText:"Visual Studio"}),(0,d.jsx)(c,{delay:".3s",imgRef:u,altText:"React"}),(0,d.jsx)(c,{delay:".35s",imgRef:f,altText:"Github"}),(0,d.jsx)(c,{delay:".4s",imgRef:b,altText:"Firebase"}),(0,d.jsx)(c,{delay:".45s",imgRef:v,altText:"NPM"})]})}),(0,d.jsx)("br",{}),(0,d.jsx)(P,{details:U}),(0,d.jsx)(P,{details:$}),(0,d.jsx)(P,{details:E}),(0,d.jsx)(P,{details:V})]}),S=(s.ZP.a`
+color: ${l.V.tertiary};
+
+&:hover{
+    color: ${l.V.primary};
+  }
+`,(0,s.ZP)(n.JX)`
+margin-left:auto;
+margin-right:auto;
+display:block;
+text-align:center;
+z-index: 2;
+`)}}]);
+//# sourceMappingURL=110.ef274dac.chunk.js.map
